6b6c02a98ba84cebf65cc9b4e7c2fb93b812d77d
Yali Zhu <yzhu@magicleap.com>
2020-01-08 15:04:08 -0500

    Version 2.0 single parent version

--- homequery.js
+++ homequery.js
@@ -2,6 +2,8 @@ var http = require('http');
 var os = require( 'os' );
 var url = require('url');
 var fs = require('fs');
+var readline = require('readline');
+
 
 var mystyle="";
 var myscript="";
@@ -18,46 +20,98 @@ function loadscript(filename){
   });
 }
 
+var array;     //line array loaded from inf file
+function loadinffile(){
+  fs.readFile('infomgr.inf', function(err, data) {
+    if(err) throw err;
+    array = data.toString().split("\n");
+    load2array();
+  });
+}
+function load2array(){
+  mystuff=[];   //reset array before loading from line array
+  for(i in array) {
+     //console.log("making item #"+i);
+     if (array[i]){
+       makeitem(array[i]);
+     }
+  }
+}
+var mystuff=[];
+function makeitem(line){
+    var string = line.split("^");
+    var id=string[0];
+    var pid=string[1];   //parentid
+    var name=string[2];
+    var desp=string[3];
+    var link=string[4];
+    mystuff.push(string);
+}
+function showitem(res,string){
+   var id=string[0];
+   var pid=string[1];   //parentid
+   if (nokids(res,id,string[2])){
+      res.write( "{name:'"+string[2]+"-"+id+"="+pid+"'},");
+   }
+}
+function nokids(res,id,pname){
+  var flag=true;    //if no var here, flag is going to be shared among the stacked instances of the function
+  var parentshown=false;
+  for(i in mystuff) {   //loop through all
+     if (mystuff[i]){   //if not empty
+       myobj=mystuff[i];
+       if(id == myobj[1]){    //check if id match any parent id
+         flag=false;          //flag as having kids
+         delete mystuff[i];   //delete
+         if(!flag && !parentshown){
+           res.write("{ name: '"+pname+"',  open: false,  type: Tree.FOLDER,  selected: false,  children: [ ");
+           parentshown=true;
+         }
+         showitem(res,myobj);
+       }
+     }
+  }
+  if(!flag){
+    res.write("]");
+    res.write("},");
+  }
+  return flag;
+}
+
 function requestHandler(req, res) {
-  //console.log("Yali CSS:"+ mystyle);
-  //console.log("Yali js: "+ myscript);
+  //console.log("mystuff====>"+mystuff);
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<!DOCTYPE html>');
   res.write('<html><head>');
-
   res.write('<style>');
   res.write(mystyle);     //style sheet printed here
   res.write('</style>');
- 
   res.write('</head><body>');
   res.write(  '<div id="tree"></div> <script>');
-
   res.write(myscript);
-
   res.write( '</script> <script>\'use strict\';');
   res.write(  " var tree = new Tree(document.getElementById('tree'), { navigate: true });");
   res.write(   'var structure = [');
-  res.write(      "{  name: 'file 1x'},");
-  res.write(      "{  name: 'file 2x'},");
-  res.write(      "{  name: 'folder 1',  open: false,  type: Tree.FOLDER,  selected: false,");
-  res.write(      "     children: [{    name: 'file 1/1'  },");
-  res.write(      "                {    name: 'file 1/2'  }");
-  res.write("]},{");
-  res.write("name: 'folder 2 (asynced)',");
-  res.write("type: Tree.FOLDER,");
-  res.write("asynced: true");
-  res.write(   '}];');
+  //========================================
+  for(i in mystuff) {
+     var item=mystuff[i];
+     delete mystuff[i];
+     showitem(res,item);
+  }
+ // =======================================
+  res.write(   '];');
   res.write(   'tree.json(structure);');
   res.write('</script></body></html>');
   res.end();
-  console.log("debug6");
+  console.log("Page done");
+  loadinffile();    //reload inf file for next request
 }
 
 loadscript("tree.js");
 loadstylesheet("tree.css");
-
+loadinffile();
 var server = http.createServer(requestHandler).listen(8081);
-console.log("homequery ver 1.00");
+console.log("homequery ver 1.01");
 
 var networkInterfaces = os.networkInterfaces( );
 //var myip=networkInterfaces['Wi-Fi'][1]['address'];
